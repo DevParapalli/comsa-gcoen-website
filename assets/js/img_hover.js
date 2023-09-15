76230364d3ff919979c3d523e8d91a4927c501c0
IshadP <ishadpande16@gmail.com>
2023-09-15 16:38:25 +0530

    some more gallery changes

--- assets/js/img_hover.js
+++ assets/js/img_hover.js
@@ -1,11 +1,11 @@
-const imgContent = document.querySelectorAll('.img-content-hover');
+// const imgContent = document.querySelectorAll('.img-content-hover');
 
-function showImgContent(e) {
-  for(var i = 0; i < imgContent.length; i++) {
-    x = e.pageX;
-    y = e.pageY;
-    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
-  }
-};
+// function showImgContent(e) {
+//   for(var i = 0; i < imgContent.length; i++) {
+//     x = e.pageX;
+//     y = e.pageY;
+//     imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+//   }
+// };
 
-document.addEventListener('mousemove', showImgContent);
+// document.addEventListener('mousemove', showImgContent);
